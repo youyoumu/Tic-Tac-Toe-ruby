9bc4f1a1fd9b147f660b91599f6588476247cb4c
yym2 <19760884+youyoumu@users.noreply.github.com>
2023-10-19 17:44:13 +0700

    Complete all tictactoe functionality

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -2,62 +2,84 @@
 
 # class for player1 and player2
 class Player
-  attr_accessor :name
+  attr_accessor :name, :mark, :is_win
+
+  @@display_array = Array.new(9, ' ')
 
   def initialize(name)
     @name = name
+    @is_win = false
   end
 
   def take_turn
     print "#{@name}'s turn: "
-    selection = gets.chomp
-
-  end
-end
-
-# display class
-class Display
-  attr_accessor :display_array
-
-  def initialize
-    @display_array = Array.new(9, '')
+    selection = gets.chomp.to_i
+    if Array(1..9).include?(selection)
+      draw_on(selection, @mark)
+    else
+      puts 'Invalid input!'
+      take_turn
+    end
   end
 
-  def draw_on(selection)
-
+  def draw_on(selection, mark)
+    if @@display_array[selection - 1] == ' '
+      @@display_array[selection - 1] = mark
+      Player.generate_display
+      check_status
+    else
+      puts "You can't draw on that grid!"
+      take_turn
+    end
   end
 
-  def generate_display
-    puts "#{@display_array[0]} | #{@display_array[1]} | #{@display_array[2]}"
+  def self.generate_display
+    puts "#{@@display_array[0]} | #{@@display_array[1]} | #{@@display_array[2]}"
     puts '--+---+--'
-    puts "#{@display_array[3]} | #{@display_array[4]} | #{@display_array[5]}"
+    puts "#{@@display_array[3]} | #{@@display_array[4]} | #{@@display_array[5]}"
     puts '--+---+--'
-    puts "#{@display_array[6]} | #{@display_array[7]} | #{@display_array[8]}"
+    puts "#{@@display_array[6]} | #{@@display_array[7]} | #{@@display_array[8]}"
+  end
+
+  def check_status
+    if (@@display_array[0] == 'X' && @@display_array[1] == 'X' && @@display_array[2] == 'X') ||
+       (@@display_array[3] == 'X' && @@display_array[4] == 'X' && @@display_array[5] == 'X') ||
+       (@@display_array[6] == 'X' && @@display_array[7] == 'X' && @@display_array[8] == 'X') ||
+       (@@display_array[0] == 'X' && @@display_array[3] == 'X' && @@display_array[6] == 'X') ||
+       (@@display_array[1] == 'X' && @@display_array[4] == 'X' && @@display_array[7] == 'X') ||
+       (@@display_array[2] == 'X' && @@display_array[5] == 'X' && @@display_array[8] == 'X') ||
+       (@@display_array[0] == 'X' && @@display_array[4] == 'X' && @@display_array[8] == 'X') ||
+       (@@display_array[2] == 'X' && @@display_array[4] == 'X' && @@display_array[6] == 'X') ||
+
+       (@@display_array[0] == 'O' && @@display_array[1] == 'O' && @@display_array[2] == 'O') ||
+       (@@display_array[3] == 'O' && @@display_array[4] == 'O' && @@display_array[5] == 'O') ||
+       (@@display_array[6] == 'O' && @@display_array[7] == 'O' && @@display_array[8] == 'O') ||
+       (@@display_array[0] == 'O' && @@display_array[3] == 'O' && @@display_array[6] == 'O') ||
+       (@@display_array[1] == 'O' && @@display_array[4] == 'O' && @@display_array[7] == 'O') ||
+       (@@display_array[2] == 'O' && @@display_array[5] == 'O' && @@display_array[8] == 'O') ||
+       (@@display_array[0] == 'O' && @@display_array[4] == 'O' && @@display_array[8] == 'O') ||
+       (@@display_array[2] == 'O' && @@display_array[4] == 'O' && @@display_array[6] == 'O')
+
+      @is_win = true
+    end
+
+    return unless @is_win
+
+    puts "#{@name} wins"
   end
 end
 
-# game class
-class Game
+puts 'Enter Player 1 Name: '
+player1 = Player.new(gets.chomp)
+player1.mark = 'X'
+puts 'Enter Player 2 Name: '
+player2 = Player.new(gets.chomp)
+player2.mark = 'O'
+Player.generate_display
 
-  def start_game
+until player1.is_win || player2.is_win
+  player1.take_turn
+  break if player1.is_win || player2.is_win
 
-  end
-
-  def player1_turn
-
-
-  end
-
-  def player2_turn
-
-  end
+  player2.take_turn
 end
-
-d1 = Display.new
-g1 = Game.new
-print 'Enter Player 1 Name: '
-player1 = Player.new("yym")
-print 'Enter Player 2 Name: '
-player2 = Player.new("ddm")
-player1.take_turn
-p d1.display_array
